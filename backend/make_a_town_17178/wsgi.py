c2485d50a6b53aa21f5054bcafea1f08cd43dc73
Crowdbotics <team@crowdbotics.com>
2020-05-20 03:03:02 +0000

    Update models

--- backend/make_a_town_17178/wsgi.py
+++ backend/make_a_town_17178/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'make_a_town_17178.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "make_a_town_17178.settings")
 
 application = get_wsgi_application()
